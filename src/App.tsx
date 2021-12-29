7f0f0e7c482a3d2f5469638b6008c7c6efce1ad7
dm1tro69 <dimolg22@gmail.com>
2021-12-29 21:37:36 +0200

    add react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/home-page/HomePage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
+      <BrowserRouter>
     <div className="App">
-      App
+      <Header/>
+      <Routes>
+        <Route path={'/'} element={<HomePage/>}/>
+      </Routes>
     </div>
+      </BrowserRouter>
   );
 }
 
